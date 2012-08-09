ca2d38f344c3edb3836707b2c33d603ef42f2f65
gerald <fckawe@googlemail.com>
2012-08-09 18:21:46 +0200

    Initial import of a blank project.

---
+++ src/com/kluckerz/Main.java
@@ -0,0 +1,21 @@
+package com.kluckerz;
+
+import com.jme3.app.SimpleApplication;
+
+/**
+ * Main class of the game.
+ * @author Gerald Backmeister <kluckerz@backmeister.name>
+ */
+public class Main extends SimpleApplication {
+    
+    public static void main(final String[] args) {
+        Main app = new Main();
+        app.start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+        // TODO:
+    }
+    
+}
